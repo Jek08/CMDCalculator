e5f18347a9e5e42a75f87f8047df6b9e21277e5e
Jaka Pratama <jk.pratama86@gmail.com>
2021-07-24 07:19:51 +0700

    v.03

--- Calculator/Calculator.cpp
+++ Calculator/Calculator.cpp
@@ -5,101 +5,87 @@ class Token
 public:
 	char kind;
 	double value;
-private:
-	Token ts.get();
-	double expression();
-	double term();
-	double primary();
+	
+	// constructors
+	Token(char ch, double val = 0.0) : kind(ch), value(val) {}
 };
 
 class Token_stream
 {
 public:
-	Token_stream();
-	Token get();
-	void putback(Token t);
+	// Constructor
+	Token_stream() : full(false), buffer(' ') {}
+	Token get();				// get a token
+	void putback(Token t);		// put a Token back to stream buffer
 private:
-
+	bool full;					// is there a Token in the buffer?
+	Token buffer;				// where we keep a token from putback()
 };
 
-double Token::expression()
+// putback() method puts its argument back to the Token_stream buffer
+void Token_stream::putback(Token t)
 {
-	double left = term();
-	Token t = ts.get();
-	while (true)
-	{
-		switch (t.kind)
-		{
-		case '+':
-			left += term();
-			t = ts.get();
-			break;
-		case '-':
-			left -= term();
-			t = ts.get();
-			break;
-		default:
-			ts.putback(t);
-			return left;
-		}
-	}
+	// precondition check
+	if (full) error("putback() into a full buffer");
+	buffer = t;		// copy t to buffer
+	full = true;	// now buffer is full
 }
 
-double Token::term()
+Token Token_stream::get()
 {
-	double left = primary();
-	Token t = ts.get();
-	while (true)
+	Token temp{ ' ' };	// temp storage for the returns
+
+	// do we already have a Token ready?
+	if (full)
+	{	
+		// remove token from buffer
+		full = false;
+		return buffer;
+	}
+	else
 	{
-		switch (t.kind)
+		char ch;
+		cin >> ch;
+
+		switch (ch)
 		{
-		case '*':
-			left *= primary();
-			t = ts.get();
+		case ';':	// end an expression
+		case 'q':	// quit program
+		case '(': case ')': case '+': case '-': case '*': case '/':
+			temp = Token(ch);
 			break;
-		case '/':
+		case '.':
+		case '0': case '1': case '2': case '3': case '4':
+		case '5': case '6': case '7': case '8': case '9':
 		{
-			double d = primary();
-			if (d == 0) error("divide by zero");
-			left /= d;
-			t = ts.get();
-			break;
+			cin.putback(ch);			// put a digit back to the input stream
+			double val;	
+			cin >> val;					// read a floating-point number
+			return Token{ '8', val };	// '8' represent a number
 		}
 		default:
-			return left;
-			break;
+			error("Bad token");
 		}
 	}
+	return temp;
 }
 
-double Token::primary()
-{
-	Token t = ts.get();
-	switch (t.kind)
-	{
-	case '(':
-	{
-		double d = expression();
-		t = ts.get();
-		if (t.kind != ')') error("')' expected");
-		return d;
-		break;
-	}
-	case '8':
-		return t.value;
-	default:
-		error("primary expected");
-	}
-}
+Token_stream ts;		// provides get() and putback()
+// deal with + and -
+double expression();	// declaration so that primary() can call expression()
+// deal with *, /, and %
+double term();	 
+// deal with numbers and parentheses
+double primary();
 
 int main()
 {
-	try 
+	try
 	{
 		double val = 0;
 		while (cin)
 		{
-			token t = ts.get();
+			Token t = ts.get();
 			if (t.kind == 'q') break;
 			if (t.kind == ';')
 				cout << "=" << val << '\n';
@@ -122,4 +108,81 @@ int main()
 		return 2;
 	}
 	return 0;
+}
+
+// deal with + and -
+double expression()
+{
+	double left = term();	// read and evaluate a Term
+	Token t = ts.get();		// get the next token from token stream
+
+	while (true)
+	{
+		switch (t.kind)
+		{
+		case '+':
+			left += term();	// evaluate Term then add
+			t = ts.get();
+			break;
+		case '-':
+			left -= term();	// evaluate Term then substract
+			t = ts.get();
+			break;
+		default:
+			ts.putback(t);	// put t back into the token stream
+			return left;	// no more - or +. return the answer
+		}
+	}
+}
+
+// deal with *, /, %
+double term()
+{
+	double left = primary();
+	Token t = ts.get();		// get the next token from token stream
+
+	while (true)
+	{
+		switch (t.kind)
+		{
+		case '*':
+			left *= primary();
+			t = ts.get();
+			break;
+		case '/':
+		{
+			double d = primary();
+			if (d == 0) error("divide by zero");
+			left /= d;
+			t = ts.get();
+			break;
+		}
+		default:
+			ts.putback(t);	// put t back to token stream
+			return left;
+		}
+	}
+}
+
+// deal with numbers and parentheses
+double primary()
+{
+	double temp{};	// temp storage for the returns
+
+	Token t = ts.get();
+	switch (t.kind)
+	{
+	case '(':		// handle '(' expression ')'
+	{
+		double d = expression();
+		t = ts.get();
+		if (t.kind != ')') error("')' expected");
+		return d;	// return expression result to caller
+		break;
+	}
+	case '8':		// '8' represent numbers
+		return t.value;	// return the number value to caller
+	default:
+		error("primary expected");
+	}
 }
